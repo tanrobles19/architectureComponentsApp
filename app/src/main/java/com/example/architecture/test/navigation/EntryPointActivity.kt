c09a17ba81bc3ef94fca09d999439c37bd212bc3
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-04-06 14:48:53 -0600

    moving activity implementation to fragments

--- app/src/main/java/com/example/architecture/test/navigation/EntryPointActivity.kt
+++ app/src/main/java/com/example/architecture/test/navigation/EntryPointActivity.kt
@@ -3,34 +3,13 @@ package com.example.architecture.test.navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.architecture.test.R
-import com.example.architecture.test.persistence.entity.Movie
 import com.example.architecture.test.viewmodel.MovieViewModel
 
 class EntryPointActivity : AppCompatActivity() {
 
-    private lateinit var movieViewModel: MovieViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val addMovie = findViewById<Button>(R.id.addNewMovieButton)
-//
-//        addMovie.setOnClickListener {
-//            addNewMovie()
-//        }
-//
-//        val movieDao  = AppDataBase.getInstance(applicationContext).movieDao()
-//        this.movieViewModel = MovieViewModel(movieDao)
-
-    }
-
-    fun addNewMovie() {
-
-        val movie = Movie("Matrix", "description text")
-
-        this.movieViewModel.createNewMovie(movie)
-
     }
 
 }
--- app/src/main/java/com/example/architecture/test/navigation/destination/MainFragment.kt
+++ app/src/main/java/com/example/architecture/test/navigation/destination/MainFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.architecture.test.R
+import com.example.architecture.test.persistence.AppDataBase
+import com.example.architecture.test.persistence.entity.Movie
+import com.example.architecture.test.viewmodel.MovieViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -14,16 +18,32 @@ import com.example.architecture.test.R
  */
 class MainFragment : Fragment() {
 
+    private lateinit var movieViewModel: MovieViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val addMovie = this.activity?.findViewById<Button>(R.id.addNewMovieButton)
+
+        addMovie?.setOnClickListener {
+            addNewMovie()
+        }
+
+        val application = requireNotNull(this.activity).application
+        val movieDao  = AppDataBase.getInstance(application).movieDao()
+        this.movieViewModel = MovieViewModel(movieDao)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    fun addNewMovie() {
+        val movie = Movie("Matrix", "description text")
+        this.movieViewModel.createNewMovie(movie)
     }
 
 }
