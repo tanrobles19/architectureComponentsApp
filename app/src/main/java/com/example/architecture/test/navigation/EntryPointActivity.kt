78e9733a16d6ab4296417266245783648d311b99
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-05-14 14:13:45 -0600

    setup Navigation Component + AppBar

--- app/src/main/java/com/example/architecture/test/navigation/EntryPointActivity.kt
+++ app/src/main/java/com/example/architecture/test/navigation/EntryPointActivity.kt
@@ -2,24 +2,20 @@ package com.example.architecture.test.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.architecture.test.R
 import com.example.architecture.test.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
 
 class EntryPointActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,54 +23,21 @@ class EntryPointActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
         val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_view) as NavHostFragment? ?: return
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment? ?: return
 
         // Set up Action Bar
-        val navController = host.navController
+        navController = host.navController
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        setupActionBar(navController, appBarConfiguration)
-    }
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
-    private fun setupActionBar(navController: NavController,
-                               appBarConfig : AppBarConfiguration
-    ) {
-        // TODO STEP 9.6 - Have NavigationUI handle what your ActionBar displays
-//        // This allows NavigationUI to decide what label to show in the action bar
-//        // By using appBarConfig, it will also determine whether to
-//        // show the up arrow or drawer menu icon
-        setupActionBarWithNavController(navController, appBarConfig)
-        // TODO END STEP 9.6
-    }
+    }// end fun onCreate()
 
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val retValue = super.onCreateOptionsMenu(menu)
-        val navigationView = findViewById<NavigationView>(R.id.nav_graph)
-        // The NavigationView already has these same navigation items, so we only add
-        // navigation items to the menu here if there isn't a NavigationView
-        if (navigationView == null) {
-            menuInflater.inflate(R.menu.menu, menu)
-            return true
-        }
-        return retValue
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return super.onOptionsItemSelected(item)
-        // TODO STEP 9.2 - Have Navigation UI Handle the item selection - make sure to delete
-        //  the old return statement above
-//        // Have the NavigationUI look for an action or destination matching the menu
-//        // item id and navigate there if found.
-//        // Otherwise, bubble up to the parent.
-        return item.onNavDestinationSelected(findNavController(R.id.nav_view))
-                || super.onOptionsItemSelected(item)
-        // TODO END STEP 9.2
+    override fun onSupportNavigateUp(): Boolean {
+        return navController
+            .navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
