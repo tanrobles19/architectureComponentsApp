6281bd2f2cc6740b3b7ac70e4c24d5cb620441a8
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-05-19 21:26:23 -0600

    adding Composables Functions

--- app/src/main/java/com/example/architecture/test/navigation/destination/MovieListWithCompose.kt
+++ app/src/main/java/com/example/architecture/test/navigation/destination/MovieListWithCompose.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 
@@ -22,19 +25,37 @@ class MovieListWithCompose : Fragment() {
         inflater: LayoutInflater, container: ViewGroup ?, savedInstanceState: Bundle?): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Greeting()
+                Greeting("Fragment Entry Point")
+            }
+        }
+    }
+
+    @Preview(showBackground = true, name = "Text Preview")
+    @Composable
+    fun DefaultPreview() {
+        Surface(color = MaterialTheme.colors.primary) {
+            TestList()
+        }
+
+    }
+    @Composable
+    fun TestList(names: List<String> = listOf("Eithan", "Issac", "Lu", "Jonathan")) {
+        Column(modifier = Modifier.padding(24.dp)){
+            for (name in names) {
+                Text(text = "Hello")
+                Text(text = name)
             }
         }
     }
 
     @Composable
-    private fun Greeting() {
+    private fun Greeting( name: String) {
         Text(
-            text = "Hello Eithan",
-            style = MaterialTheme.typography.h5,
+            text = name,
+            style = MaterialTheme.typography.h6,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 8.dp)
+                .padding(24.dp)
                 .wrapContentWidth(Alignment.CenterHorizontally)
         )
     }
