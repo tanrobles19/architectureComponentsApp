1d7f0a0231b7b0a1d9b64ff9f8aade7c5403c4cc
Jonathan Robles <jonathan.robles@etc-latam.com>
2022-06-03 09:09:11 -0600

    fixing issues wit configuration changes
    Replace remember with rememberSaveable

--- app/src/main/java/com/example/architecture/test/composable/WellnessTaskItem.kt
+++ app/src/main/java/com/example/architecture/test/composable/WellnessTaskItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -40,7 +41,7 @@ fun WellnessTaskItem(
 
 @Composable
 fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
-    var checkedState by remember { mutableStateOf(false)}
+    var checkedState by rememberSaveable { mutableStateOf(false)}
 
     WellnessTaskItem(
         taskName = taskName,
